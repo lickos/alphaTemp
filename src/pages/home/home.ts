34b90630462f69c9835bd70583e77ba1c5886183
lickos <george.chios@gmail.com>
2017-09-26 14:41:41 +0300

    Home

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -17,6 +17,7 @@ export class HomePage {
   isInFavs3: boolean = false;
   isInFavs4: boolean = false;
   isInFavs5: boolean = false;
+  isInFavsEd: boolean = false;
   showCarouzel: boolean = true;
   showBreaking: boolean = false;
   breakingNews: any;
@@ -150,140 +151,123 @@ export class HomePage {
 
   ionViewDidLoad() {
     // "https://alphanews.live/breaking.json"
-    this.getData
-      .getRemoteData("https://alphanews.live/breaking.json")
-      .then(data => {
-        this.top0 = data[0];
-        let classType0 = data[0].category;
-        let breaking = data[0].breaking;
-        let brNid = data[0].nid;
-        this.class0 = this.selectClass(classType0);
-        if (breaking == "on") {
-          this.showBreaking = true;
-          this.showCarouzel = false;
-          this.callBreaking(brNid);
-        }
-        this.strgprvd.checkIfInfavs(this.top0.nid).then(val => {
-          this.isInFavs0 = val;
-        });
-        this.top1 = data[1];
-        let classType1 = data[1].category;
-        this.class1 = this.selectClass(classType1);
-        this.strgprvd.checkIfInfavs(this.top1.nid).then(val => {
-          this.isInFavs1 = val;
-        });
-        this.top2 = data[2];
-        let classType2 = data[2].category;
-        this.class2 = this.selectClass(classType2);
-        this.strgprvd.checkIfInfavs(this.top2.nid).then(val => {
-          this.isInFavs2 = val;
-        });
-        this.top3 = data[3];
-        let classType3 = data[3].category;
-        this.class3 = this.selectClass(classType3);
-        this.strgprvd.checkIfInfavs(this.top3.nid).then(val => {
-          this.isInFavs3 = val;
-        });
-        this.top4 = data[4];
-        let classType4 = data[4].category;
-        this.class4 = this.selectClass(classType4);
-        this.strgprvd.checkIfInfavs(this.top4.nid).then(val => {
-          this.isInFavs4 = val;
-        });
-        this.top5 = data[5];
-        let classType5 = data[5].category;
-        this.class5 = this.selectClass(classType5);
-        this.strgprvd.checkIfInfavs(this.top5.nid).then(val => {
-          this.isInFavs5 = val;
-        });
+    this.getData.getRemoteData("https://alphanews.live/json/topnews").then(data => {
+      this.top0 = data[0];
+      let classType0 = data[0].category;
+      let breaking = data[0].breaking;
+      let brNid = data[0].nid;
+      this.class0 = this.selectClass(classType0);
+      if (breaking == "on") {
+        this.showBreaking = true;
+        this.showCarouzel = false;
+        this.callBreaking(brNid);
+      }
+      this.strgprvd.checkIfInfavs(this.top0.nid).then(val => {
+        this.isInFavs0 = val;
       });
-    this.getData
-      .getRemoteData("http://alphanews.live/json/editorials")
-      .then(data => {
-        this.editor = data[2];
+      this.top1 = data[1];
+      let classType1 = data[1].category;
+      this.class1 = this.selectClass(classType1);
+      this.strgprvd.checkIfInfavs(this.top1.nid).then(val => {
+        this.isInFavs1 = val;
       });
-    this.getData
-      .getRemoteData("https://alphanews.live/json/cat/1")
-      .then(data => {
-        this.CypData = data[0];
-        this.CypData1 = data[1];
-        this.CypData2 = data[2];
-        this.pic0 = data[0].image_url;
-        this.storage.set("CypData", data).then(() => {
-          console.log("Success Storage Cyp");
-        });
+      this.top2 = data[2];
+      let classType2 = data[2].category;
+      this.class2 = this.selectClass(classType2);
+      this.strgprvd.checkIfInfavs(this.top2.nid).then(val => {
+        this.isInFavs2 = val;
       });
-    this.getData
-      .getRemoteData(" https://alphanews.live/json/cat/2")
-      .then(data => {
-        this.PolData = data[0];
-        this.PolData1 = data[1];
-        this.PolData2 = data[2];
-        this.pic1 = data[0].image_url;
-        this.storage.set("PolData", data).then(() => {
-          console.log("Success Storage Pol");
-        });
+      this.top3 = data[3];
+      let classType3 = data[3].category;
+      this.class3 = this.selectClass(classType3);
+      this.strgprvd.checkIfInfavs(this.top3.nid).then(val => {
+        this.isInFavs3 = val;
       });
-    this.getData
-      .getRemoteData(" https://alphanews.live/json/cat/6")
-      .then(data => {
-        this.SportsData = data[0];
-        this.SportsData1 = data[1];
-        this.SportsData2 = data[2];
-        this.pic2 = data[0].image_url;
-        this.storage.set("SportsData", data).then(() => {
-          console.log("Success Storage Sports");
-        });
+      this.top4 = data[4];
+      let classType4 = data[4].category;
+      this.class4 = this.selectClass(classType4);
+      this.strgprvd.checkIfInfavs(this.top4.nid).then(val => {
+        this.isInFavs4 = val;
       });
-    this.getData
-      .getRemoteData(" https://alphanews.live/json/cat/7")
-      .then(data => {
-        this.EntData = data[0];
-        this.EntData1 = data[1];
-        this.EntData2 = data[2];
-        this.pic3 = data[0].image_url;
-        this.storage.set("EntData", data).then(() => {
-          console.log("Success Storage Ent");
-        });
+      this.top5 = data[5];
+      let classType5 = data[5].category;
+      this.class5 = this.selectClass(classType5);
+      this.strgprvd.checkIfInfavs(this.top5.nid).then(val => {
+        this.isInFavs5 = val;
       });
-    this.getData
-      .getRemoteData(" https://alphanews.live/json/cat/5")
-      .then(data => {
-        this.IntData = data[0];
-        this.IntData1 = data[1];
-        this.IntData2 = data[2];
-        this.pic4 = data[0].image_url;
-        this.storage.set("IntData", data).then(() => {
-          console.log("Success Storage Int");
-        });
+    });
+    this.getData.getRemoteData("http://alphanews.live/json/editorials").then(data => {
+      this.editor = data[0];
+      this.strgprvd.checkIfInfavs(this.editor.nid).then(val => {
+        this.isInFavsEd = val;
       });
-    this.getData
-      .getRemoteData(" https://alphanews.live/json/cat/4")
-      .then(data => {
-        this.GreeceData = data[0];
-        this.GreeceData1 = data[1];
-        this.storage.set("GreeceData", data).then(() => {
-          console.log("Success Storage Greece");
-        });
+    });
+    this.getData.getRemoteData("https://alphanews.live/json/cat/1").then(data => {
+      this.CypData = data[0];
+      this.CypData1 = data[1];
+      this.CypData2 = data[2];
+      this.pic0 = data[0].image_url;
+      this.storage.set("CypData", data).then(() => {
+        console.log("Success Storage Cyp");
       });
-    this.getData
-      .getRemoteData(" https://alphanews.live/json/cat/8")
-      .then(data => {
-        this.HealthData = data[0];
-        this.HealthData1 = data[1];
-        this.storage.set("HealthData", data).then(() => {
-          console.log("Success Health");
-        });
+    });
+    this.getData.getRemoteData(" https://alphanews.live/json/cat/2").then(data => {
+      this.PolData = data[0];
+      this.PolData1 = data[1];
+      this.PolData2 = data[2];
+      this.pic1 = data[0].image_url;
+      this.storage.set("PolData", data).then(() => {
+        console.log("Success Storage Pol");
       });
-    this.getData
-      .getRemoteData(" https://alphanews.live/json/cat/9")
-      .then(data => {
-        this.EconomyData = data[0];
-        this.EconomyData1 = data[1];
-        this.storage.set("EconomyData", data).then(() => {
-          console.log("Success Economy");
-        });
+    });
+    this.getData.getRemoteData(" https://alphanews.live/json/cat/6").then(data => {
+      this.SportsData = data[0];
+      this.SportsData1 = data[1];
+      this.SportsData2 = data[2];
+      this.pic2 = data[0].image_url;
+      this.storage.set("SportsData", data).then(() => {
+        console.log("Success Storage Sports");
       });
+    });
+    this.getData.getRemoteData(" https://alphanews.live/json/cat/7").then(data => {
+      this.EntData = data[0];
+      this.EntData1 = data[1];
+      this.EntData2 = data[2];
+      this.pic3 = data[0].image_url;
+      this.storage.set("EntData", data).then(() => {
+        console.log("Success Storage Ent");
+      });
+    });
+    this.getData.getRemoteData(" https://alphanews.live/json/cat/5").then(data => {
+      this.IntData = data[0];
+      this.IntData1 = data[1];
+      this.IntData2 = data[2];
+      this.pic4 = data[0].image_url;
+      this.storage.set("IntData", data).then(() => {
+        console.log("Success Storage Int");
+      });
+    });
+    this.getData.getRemoteData(" https://alphanews.live/json/cat/4").then(data => {
+      this.GreeceData = data[0];
+      this.GreeceData1 = data[1];
+      this.storage.set("GreeceData", data).then(() => {
+        console.log("Success Storage Greece");
+      });
+    });
+    this.getData.getRemoteData(" https://alphanews.live/json/cat/8").then(data => {
+      this.HealthData = data[0];
+      this.HealthData1 = data[1];
+      this.storage.set("HealthData", data).then(() => {
+        console.log("Success Health");
+      });
+    });
+    this.getData.getRemoteData(" https://alphanews.live/json/cat/9").then(data => {
+      this.EconomyData = data[0];
+      this.EconomyData1 = data[1];
+      this.storage.set("EconomyData", data).then(() => {
+        console.log("Success Economy");
+      });
+    });
   }
 
   setFav0(item) {
@@ -314,6 +298,11 @@ export class HomePage {
   setFav5(item) {
     this.strgprvd.setFavs(item);
     this.isInFavs5 = true;
+  }
+
+  setFavEd(item) {
+    this.strgprvd.setFavs(item);
+    this.isInFavsEd = true;
   }
 
   alertFav() {
